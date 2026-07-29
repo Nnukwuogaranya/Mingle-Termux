01008caf2e7c7a5dc922541bae44e5ecfae2e1b0
Nnukwuogaranya <deyfavouredpraise@gmail.com>
2026-07-29 07:21:07 +0100

    Add homepage to fix 404

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{textAlign: 'center', padding: '50px'}}>
+      <h1>Welcome to Mingle</h1>
+      <a href="/login" style={{color: 'blue', fontSize: '20px'}}>Go to Login →</a>
+    </div>
+  )
+}
