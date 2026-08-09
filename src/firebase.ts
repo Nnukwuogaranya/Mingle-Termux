4888049c53cc0b9db764af581dfd74b29f7f1374
Nnukwuogaranya <deyfavouredpraise@gmail.com>
2026-08-09 09:40:46 +0100

    Connect React app to Firebase Auth

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,8 +1,6 @@
-// Import the functions you need from the Firebase SDK
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBNOdtu-QEFF9ft9S95arVVG1gNjZJm-EE",
   authDomain: "mingle-9c827.firebaseapp.com",
@@ -12,11 +10,6 @@ const firebaseConfig = {
   appId: "1:617137584934:web:5f2a4dd470862394135fe7",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Export Authentication
 export const auth = getAuth(app);
-
-
-export { app };
